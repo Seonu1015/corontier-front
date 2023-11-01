f70e43b50ef655d12062f60cd1a07884b30e6e92
kumhee <kumheee95@gmail.com>
2023-11-01 15:41:33 +0900

    //

---
+++ src/components/project/ProjectMain.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ProjectMain = () => {
+    return (
+        <div className='page_wrap'>
+            <div className='banner'>
+                <img src="../images/banner.png" alt="" />
+            </div>
+
+            <div className='page_contents_wrap'>
+                <div className='contents_title_box'>
+                    <p className='contents_title'>ProjectMain Page</p>
+                    <p className='contents_article'>...</p>
+                </div>
+
+                <div className='page_contents'>
+                    <div className='box1'> 1 </div>
+                    <div className='box2'> 2 </div>
+                    <div className='box3'> 3 </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectMain
