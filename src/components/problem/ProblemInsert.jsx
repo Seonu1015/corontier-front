6f85aa216fbd48839e24bc073a2c656598a6f077
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-11-08 01:18:53 +0900

    WIP: package.json 수정, 문제 등록 페이지 일부 수정 중

--- src/components/problem/ProblemInsert.jsx
+++ src/components/problem/ProblemInsert.jsx
@@ -1,13 +1,24 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { Form, Row, Col, Dropdown, Button } from 'react-bootstrap'
 import { BoxContext } from '../BoxContext'
 
 import EditorComp from '../common/EditorComp'
+import { async } from 'q'
+import axios from 'axios'
 
 const ProblemInsert = () => {
     const { setBox } = useContext(BoxContext);
     const [loading, setLoading] = useState(false);
     const [content, setContent] = useState('');
+    const [tags, setTags] = useState([]);
+
+    const getTags = async () => {
+        setLoading(true);
+        const res = await axios('/problem/tag/list.json');
+        console.log(res.data);
+        setTags(res.data);
+        setLoading(false);
+    }
 
     const onEditorChange = (data) => {
         setContent(data);
@@ -18,20 +29,55 @@ const ProblemInsert = () => {
             show: true,
             message: "등록하시겠습니까?",
             action: async () => {
-                
+
             }
         });
     }
+
+    useEffect(() => {
+        getTags();
+    }, []);
 
     return (
         <div className='my-5'>
             <h1 className='text-center mb-5'>문제 등록</h1>
             <Row className='justify-content-center'>
                 <Col md={8} className='mx-3'>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control type="text" />
-                    </Form.Group>
+                    <Row>
+                        <Col md={9}>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control type="text" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Difficulty</Form.Label>
+                                <Dropdown className='Dropdown'>
+                                    <Dropdown.Toggle variant='outline-secondary' className='title_l'>
+                                        난이도
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        
+                                        <Dropdown.Item>Easy</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Form>
+                        <div key={"inline-checkbox"} className="mt-2 mb-3">
+                            {tags.map(tag =>
+                                <Form.Check
+                                    inline
+                                    label={tag.tag_name}
+                                    name="group1"
+                                    type={"checkbox"}
+                                    id={"inline-checkbox-1"}
+                                />
+                            )}
+                        </div>
+                    </Form>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Description</Form.Label>
                         <EditorComp onChange={onEditorChange} />
@@ -46,27 +92,6 @@ const ProblemInsert = () => {
                         <Col md={4}>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                 <Form.Label>Output</Form.Label>
-                                <Form.Control type="text" />
-                            </Form.Group>
-                        </Col>
-                        <Col md={2}>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Label>Difficulty</Form.Label>
-                                <Dropdown className='Dropdown'>
-                                    <Dropdown.Toggle variant='outline-secondary' className='title_l'>
-                                        난이도
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item>Easy</Dropdown.Item>
-                                        <Dropdown.Item>Middel</Dropdown.Item>
-                                        <Dropdown.Item>Hard</Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </Form.Group>
-                        </Col>
-                        <Col md={2}>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Label>Tag</Form.Label>
                                 <Form.Control type="text" />
                             </Form.Group>
                         </Col>
