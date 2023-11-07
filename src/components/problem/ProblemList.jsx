6a688b39275f85e0a4a28d6978821d08b58347c4
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-11-08 00:37:53 +0900

    Fix: 문제 리스트 페이지 중복 출력 수정

--- src/components/problem/ProblemList.jsx
+++ src/components/problem/ProblemList.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { BoxContext } from '../BoxContext';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Table } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 
 const ProblemList = () => {
     const { setBox } = useContext(BoxContext);
     const [problems, setProblems] = useState([]);
     const [loading, setLoading] = useState(false);
+
+    const navi = useNavigate();
 
     const size = 20;
     const location = useLocation();
@@ -19,13 +21,7 @@ const ProblemList = () => {
         setLoading(true);
         const res = await axios(`/problem/list.json?page=${page}&size=${size}`);
         const list = res.data.list;
-        for (let i = 0; i < list.length; i++) {
-            const buffer = list[i].content.data;
-            const uintArray = new Uint8Array(buffer);
-            const content = new TextDecoder().decode(uintArray);
-            list[i].content = content;
-        }
-        // console.log(list);
+        console.log(list);
         setProblems(list);
         setLoading(false);
     }
@@ -37,25 +33,37 @@ const ProblemList = () => {
     return (
         <div className='m-5'>
             <h1 className='text-center mb-5'>문제 리스트</h1>
-            <Table hover className='text-center justify-content-center'>
+            <Table hover className='problemList text-center'>
                 <thead>
                     <tr>
-                        <th width="10%">난이도</th>
-                        <th width="40%">제목</th>
-                        <th width="10%">정답</th>
-                        <th width="10%">제출</th>
-                        <th width="10%">정답률</th>
+                        <th width="8%">난이도</th>
+                        <th>제목</th>
+                        <th width="18%">등록일</th>
+                        <th width="10%">수정</th>
+                        <th width="10%">삭제</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {problems.map(p => 
-                        <tr key={p.problem_id}>
-                            <td>{p.grade}</td>
-                            <td className='text-start px-3'><NavLink to="">{p.title}</NavLink></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>    
+                    {problems.map(p =>
+                        <>
+                            <tr key={p.problem_id}>
+                                <td style={{verticalAlign:"middle"}}>{p.grade}</td>
+                                <td className='text-start ps-5'>
+                                    <div>{p.title}</div>
+                                    <div style={{ fontSize: "70%" }}>{p.tag_names}</div>
+                                </td>
+                                <td style={{verticalAlign:"middle"}}>
+                                    <div>{p.fmt_created}</div>
+                                    <div style={{ fontSize: "70%" }}>수정 : {p.fmt_updated}</div>
+                                </td>
+                                <td style={{verticalAlign:"middle"}}>
+                                    <Button size='sm' onClick={()=> navi(`/problem/update/${p.problem_id}`)}>수정</Button>
+                                </td>
+                                <td style={{verticalAlign:"middle"}}>
+                                    <Button variant='danger' size='sm'>삭제</Button>
+                                </td>
+                            </tr>
+                        </>
                     )}
                 </tbody>
             </Table>
