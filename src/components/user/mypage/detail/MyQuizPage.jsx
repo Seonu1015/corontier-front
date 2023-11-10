5cd584455f5c5e777185697f27323a82905f43e4
gwoneu <gwoneunji9634@gmail.com>
2023-11-10 09:58:57 +0900

    Feat : MyQuizPage UI 구현

--- src/components/user/mypage/detail/MyQuizPage.jsx
+++ src/components/user/mypage/detail/MyQuizPage.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import "../../../../css/Mypage.css"
+import { Table } from 'react-bootstrap'
 
 const MyQuizPage = () => {
   return (
-    <div className='contents'>MyQuizPage</div>
+    <div className='page_wrap'>
+      <div className='myquizpage_wrap'>
+        <div className='myquizpage_title'>
+          <p>My Quiz</p>
+        </div>
+        <div className='myquizpage_recentquizgroup'>
+          <div className='myquizpage_recentquiz'>
+            <Table className='myquizpage_recenttable'>
+              <thead>
+                <tr>
+                  <th width="10%">No</th>
+                  <th width="60%">Title</th>
+                  <th width="15%">OX</th>
+                  <th width="15%">풀이 갯수</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* 반복되는 출력되는 값*/}
+                <tr className='myquizpage_recentquiz_list'>
+                  <td>01</td>
+                  <td>누가 사과를 먹었을까?</td>
+                  <td>O</td>
+                  <td>2</td>
+                </tr>
+                <tr className='myquizpage_recentquiz_list'>
+                  <td>01</td>
+                  <td>누가 사과를 먹었을까?</td>
+                  <td>O</td>
+                  <td>2</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
