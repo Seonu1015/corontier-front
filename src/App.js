bfc78264df11bdc831774c932e29f1356ff9854c
Arum <rlfehd429@gmail.com>
2023-11-10 15:08:51 +0900

    Fix: PR 충돌 해결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './css/App.css';
 import './css/default.css';
+import './css/testpage.css';
 import './css/problempage.css';
 import './css/Project.css';
 import { Container } from 'react-bootstrap'
