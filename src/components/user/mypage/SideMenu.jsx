69afce08eb4584bdefe4e31651234dea794cce32
gwoneu <gwoneunji9634@gmail.com>
2023-11-13 11:05:35 +0900

    design : sidebar UI 변경

--- src/components/user/mypage/SideMenu.jsx
+++ src/components/user/mypage/SideMenu.jsx
@@ -7,24 +7,6 @@ import { Button } from 'react-bootstrap';
 const SideMenu = () => {
 
   return (
-  //   <div className='side-bar'>
-  //     <div className='side_userimg'>
-  //       <img src={'http://via.placeholder.com/200x200'} />
-  //     </div>
-  //     <div className='side_userid'>
-  //       <p>USERID</p>
-  //     </div>
-  //     <div className='side_bar_menu'>
-  //       <ul>
-  //         <Link to=''><Button variant='outline-secondary'>My Quize</Button></Link>
-  //         <Link to='oxnote'><Button variant='outline-secondary'>오답노트</Button></Link>
-  //         <Link to='mylist'><Button variant='outline-secondary'>My List</Button></Link>
-  //         <Link to='myactivity'><Button variant='outline-secondary'>나의활동</Button></Link>
-  //         <Link to='userupdate'><Button variant='outline-secondary'>개인정보수정</Button></Link>
-  //         <Button variant='outline-danger'>로그아웃</Button>
-  //     </ul>
-  //   </div>
-  // </div >
   <div className='mypage_sidebar_wrap'>
     <div className='mypage_sidebar_box'>
       <ol className='mypage_sidebar_ol'>
